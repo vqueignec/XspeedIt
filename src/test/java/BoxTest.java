8218a01b0db63d90deec08dc7388ec56c75f6138
vqueignec <vincent.queignec@devoteam.com>
2018-07-01 15:13:01 +0200

    Test initialisation

---
+++ src/test/java/BoxTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+
+/**
+ * @author - v.queignec
+ */
+public class BoxTest {
+
+    @Test
+    public void addSimpleArticle() {
+        assertFalse(true);
+    }
+
+    @Test
+    public void addSimpleArticleBis() {
+        assertFalse(true);
+    }
+
+    @Test
+    public void addArticleExceedingCapacity(){
+        assertFalse(true);
+    }
+}
