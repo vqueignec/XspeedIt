d458b3b06165cad787f26de1460c88b08eefe1c0
vqueignec <vincent.queignec@devoteam.com>
2018-07-01 15:24:38 +0200

    Box Implementation commit with tests passing

---
+++ src/main/java/Box.java
@@ -0,0 +1,30 @@
+import exceptions.ArticleDoesntFitException;
+import lombok.Data;
+
+import java.util.ArrayList;
+
+/**
+ * @author - v.queignec
+ */
+@Data
+public class Box {
+
+    private ArrayList<Integer> articles = new ArrayList<>();
+    private int freePlace = 10;
+
+    private int getUsedPlace(){
+        return articles.stream().mapToInt(value -> value).sum();
+    }
+
+
+    private boolean doesArticleFit(int article){
+        return getFreePlace() >= article;
+    }
+
+    public void addArticle(int article) throws ArticleDoesntFitException {
+        if(!doesArticleFit(article))
+            throw new ArticleDoesntFitException();
+        articles.add(article);
+        freePlace -= article;
+    }
+}
--- src/test/java/BoxTest.java
+++ src/test/java/BoxTest.java
@@ -1,5 +1,12 @@
+import exceptions.ArticleDoesntFitException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 
 
@@ -8,18 +15,43 @@ import static org.junit.Assert.assertFalse;
  */
 public class BoxTest {
 
+    @Rule
+    public ExpectedException exceptionGrabber = ExpectedException.none();
+
     @Test
-    public void addSimpleArticle() {
-        assertFalse(true);
+    public void addSimpleArticle() throws ArticleDoesntFitException {
+        Box box = givenEmptyBox();
+
+        box.addArticle(5);
+        assertThat(box.getFreePlace(), is(5));
     }
 
     @Test
-    public void addSimpleArticleBis() {
-        assertFalse(true);
+    public void addSimpleArticleBis() throws ArticleDoesntFitException {
+        Box box = givenEmptyBox();
+
+        box.addArticle(7);
+        assertThat(box.getFreePlace(), is(3));
     }
 
     @Test
-    public void addArticleExceedingCapacity(){
-        assertFalse(true);
+    public void addArticleExceedingCapacity() throws ArticleDoesntFitException {
+        Box box = givenEmptyBox();
+
+        box.addArticle(7);
+        exceptionGrabber.expect(ArticleDoesntFitException.class);
+        box.addArticle(7);
+    }
+
+    private Box givenEmptyBox(){
+        return new Box();
+    }
+
+    private Box givenBoxWithArticles(List<Integer> articles) throws ArticleDoesntFitException {
+        Box result = new Box();
+        for (Integer article: articles) {
+            result.addArticle(article);
+        }
+        return result;
     }
 }
